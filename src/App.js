4ed5d0d532aee206c71085c34c5436f18325fb08
Adrian Rak <raczy7@gmail.com>
2020-07-19 03:44:32 +0100

    Search option included

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 
 import SearchBox from './components/search-box/search-box.component';
@@ -7,18 +7,47 @@ import ErrorBoundary from './ErrorBoundary';
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <SearchBox />
-        <ErrorBoundary>
-          <CardList />
-        </ErrorBoundary>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      filteredUsers: []
+    }
+  }
+
+  fetchProfiles = async () => {
+    try {
+        const fetchedJson = await fetch('https://jsonplaceholder.typicode.com/users');
+        const profiles = await fetchedJson.json();
+        this.setState({ users: profiles, filteredUsers: profiles });
+    } catch (error) {
+        console.log('Failed to fetch');
+    }
+  }
+
+  handleSearchChange = (event) => {
+    this.setState({ filteredUsers: event});
+  }
+
+  componentDidMount() {
+    this.fetchProfiles();
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <SearchBox users={this.state.users} onSearchChange={this.handleSearchChange}/>
+          <ErrorBoundary>
+            <CardList users={this.state.filteredUsers}/>
+          </ErrorBoundary>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
 import { TextField } from '@material-ui/core';
-import './search-box.style.css'
+import './search-box.style.css';
 
 class SearchBox extends Component {
     constructor(props) {
         super(props);
         this.state = {
             search: null
-        }
+        };
     }
 
-
+    updateSearch = (event) => {
+        this.setState({ search: event.target.value });
+        const filteredUsers = this.props.users.filter( element => {
+            return element.name.toLowerCase().includes(event.target.value.toLowerCase());
+        });
+        this.props.onSearchChange(filteredUsers);
+    }
     
     render() {
-        const updateSearch = () => {
-            
-        }
-
         return (
             <div className='Search-box'>
-                <TextField id='outlined-basic' label='Search' variant='outlined' onChange={updateSearch} />
+                <TextField id='outlined-basic' label='Search' variant='outlined' onChange={this.updateSearch} />
             </div>
         );
     }
